ce5a00fb37c3d43083dcfc0238f47711b5c0c59d
A23coder <91893285+A23coder@users.noreply.github.com>
2024-05-08 16:33:48 +0530

    Done Firebase

--- app/src/main/java/com/example/expensetracker/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/expensetracker/fragment/HomeFragment.kt
@@ -77,6 +77,7 @@ class HomeFragment : Fragment() {
         binding.imgSearch.setOnClickListener {
             navigateToTransactionFragment()
         }
+
     }
 
     //    private fun navigateToTransactionFragment() {
